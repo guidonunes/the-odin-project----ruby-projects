0a0d4dbc86c9957bba3b0e0ba0e8b501e65d8b73
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-23 06:59:14 -0300

    add lowercase message variable

--- 01_caesar_cipher/cipher.rb
+++ 01_caesar_cipher/cipher.rb
@@ -1,3 +1,25 @@
 def caesar_cipher(message, shift)
     alphabet = ('a'..'z').to_a
+    
+
+# convert message to lowercase
+    lowercase_message = message.downcase
 end
+
+
+
+
+
+
+
+
+
+
+
+
+#Steps
+
+#1. Convert the message to lowercase
+#2. Initialize empty string to store encrypted message
+#3. Iterate through each character in the lowercase message
+
