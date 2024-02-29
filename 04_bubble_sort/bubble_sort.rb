28827f4015fc53fa1a318c80d3cc154c63584323
guilhermenunes25 <guilherme.augd@gmail.com>
2024-02-29 14:30:21 -0300

    add arr_length variable

--- 04_bubble_sort/bubble_sort.rb
+++ 04_bubble_sort/bubble_sort.rb
@@ -1,3 +1,8 @@
 def bubble_sort(arr)
-
+    arr_length = arr.size
+    
 end
+
+
+# each element is compared to the one next to it
+# they are swapped if the one on the left is larger than the one on the right.  
