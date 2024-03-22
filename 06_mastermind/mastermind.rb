2f8eda0ad368832e4bf3b01785b3512bf28f728c
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-22 07:48:00 -0300

    update game instructions method

--- 06_mastermind/mastermind.rb
+++ 06_mastermind/mastermind.rb
@@ -1,6 +1,31 @@
 class Mastermind
   COLORS = ["R", "G", "B", "Y", "O", "P"].freeze
 
+  def intialize
+    game_instructions
+  end
+
+  def game_instructions
+    puts "==================== Welcome to Mastermind! ===================="
+    puts " "
+    puts "In this game, you will try to guess the secret code." 
+    puts "The secret code is a combination of colors."
+    puts "The Available colors are:"
+    puts " "
+    self.class.print_colors_with_colors
+    puts " "
+    puts "After each guess, you will receive feedback."
+    puts " "
+    puts "  - ●  means correct color in the correct position."
+    puts "  - ○  means correct color but in the wrong position."
+    puts "  - #  means incorrect color."
+    puts " "
+    puts "You have 10 attempts to crack the code. Have fun!"
+    puts " "
+    puts "=================================================================="
+  end
+
+  # ANSI scape codes to print names of the constants in different colors on the terminal
   def self.print_colors_with_colors
     COLORS.each do |color|
       case color
@@ -22,3 +47,6 @@ class Mastermind
     end    
   end
 end
+
+mastermind = Mastermind.new
+mastermind.game_instructions
