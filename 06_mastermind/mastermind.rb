b54a98755ef83da328ddeb29b0fdbf4c0165a119
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-22 09:21:24 -0300

    add method to generate secret code

--- 06_mastermind/mastermind.rb
+++ 06_mastermind/mastermind.rb
@@ -25,16 +25,20 @@ class Mastermind
     puts "=================================================================="
   end
 
+  def secret_code
+    COLORS.shuffle.take(4)
+  end
+
   # ANSI scape codes to print names of the constants in different colors on the terminal
   def self.print_colors_with_colors
     COLORS.each_with_index do |color, index|
       color_code = case index
-                    when 0 then "\e[31m" 
-                    when 1 then "\e[32m"
-                    when 2 then "\e[34m"
-                    when 3 then "\e[33m"
-                    when 4 then "\e[33m"
-                    when 5 then "\e[35m" 
+                     when 0 then "\e[31m" 
+                     when 1 then "\e[32m"
+                     when 2 then "\e[34m"
+                     when 3 then "\e[33m"
+                     when 4 then "\e[33m"
+                     when 5 then "\e[35m" 
                     else "\e[0m"
                    end
     puts "#{color_code}#{color}\e[0m"  
