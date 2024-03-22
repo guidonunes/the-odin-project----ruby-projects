134c33d22135270801fb8a9b52d23750e542db97
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-22 06:52:36 -0300

    add method to print corresponding colors

--- 06_mastermind/mastermind.rb
+++ 06_mastermind/mastermind.rb
@@ -1,5 +1,10 @@
 class Mastermind
-    COLORS = ["Red", "Green", "Blue", "Yellow", "Orange", "Purple"].freeze
+    COLORS = ["R", "G", "B", "Y", "O", "P"].freeze
 
-    
+    def self.print_colors_with_colors
+        COLORS.each do |color|
+          case color
+          when "R"
+            puts "\e[31m#{color}\e[0m"
+    end
 end
