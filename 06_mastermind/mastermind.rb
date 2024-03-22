69e8a6a0e8b9966e3977aadef094524d678fd468
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-22 07:00:31 -0300

    add switch statement

--- 06_mastermind/mastermind.rb
+++ 06_mastermind/mastermind.rb
@@ -1,10 +1,24 @@
 class Mastermind
-    COLORS = ["R", "G", "B", "Y", "O", "P"].freeze
+  COLORS = ["R", "G", "B", "Y", "O", "P"].freeze
 
-    def self.print_colors_with_colors
-        COLORS.each do |color|
-          case color
-          when "R"
-            puts "\e[31m#{color}\e[0m"
-    end
+  def self.print_colors_with_colors
+    COLORS.each do |color|
+      case color
+      when "R"
+        puts "\e[31m#{color}\e[0m"
+      when "G"
+        puts "\e[32m#{color}\e[0m"
+      when "B"
+        puts "\e[34m#{color}\e[0m"
+      when "Y"
+        puts "\e[33m#{color}\e[0m"
+      when "O"
+        puts "\e[33m#{color}\e[0m"
+      when "P"
+        puts "\e[35m#{color}\e[0m" 
+      else
+        puts color
+      end
+    end    
+  end
 end
