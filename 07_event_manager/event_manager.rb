4292dc677590bc634d3a1b6b79260e6650fb3df9
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-26 09:00:58 -0300

    add variable to store value accessed on column[2]

--- 07_event_manager/event_manager.rb
+++ 07_event_manager/event_manager.rb
@@ -2,7 +2,9 @@ puts 'EventManager initialized'
 
 lines = File.readlines('event_attendees.csv')
 lines.each do |line|
-    puts line
+    columns = line.split(",")
+    name = columns[2]
+    puts name
 end
 
 
