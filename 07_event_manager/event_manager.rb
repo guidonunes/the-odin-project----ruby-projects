1d3ed70d8859690392a14c2cef75928e61cd1ca0
guilhermenunes25 <guilherme.augd@gmail.com>
2024-03-26 09:54:59 -0300

    load csv library

--- 07_event_manager/event_manager.rb
+++ 07_event_manager/event_manager.rb
@@ -1,8 +1,10 @@
+require 'csv'
+
 puts 'EventManager initialized'
 
 lines = File.readlines('event_attendees.csv')
-
 lines.each_with_index do |line, index|
+# skip header row. It assumes that it is the first row in the file.
     next if index == 0
     columns = line.split(",")
     name = columns[2]
@@ -10,4 +12,4 @@ lines.each_with_index do |line, index|
 end
 
 
-
+# TO BE CONTINUED: Iteration 1: Parsing with CSV
