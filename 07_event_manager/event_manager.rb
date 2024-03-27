866672129c0d0c455e5a3de0fd63bbc6d35c72d8
Guilherme Nunes <guilherme.augd@gmail.com>
2024-03-27 11:21:46 -0300

    delete comments

--- 07_event_manager/event_manager.rb
+++ 07_event_manager/event_manager.rb
@@ -20,10 +20,6 @@ contents.each do |row|
       zipcode = zipcode[0..4]
     end
     
-    # if the zip code is exactly five digits, assume that it is ok
-    # if the zip code is more than five digits, truncate it to the first five digits
-    # if the zip code is less than five digits, add zeros to the front until it becomes five digits
-    
     puts "#{name} #{zipcode}"
 end
 
