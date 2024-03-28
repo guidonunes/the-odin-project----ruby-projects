ed8a9b3c911e1920dd9217694689d27e21216212
Guilherme Nunes <guilherme.augd@gmail.com>
2024-03-28 10:22:49 -0300

    add key

--- 07_event_manager/event_manager.rb
+++ 07_event_manager/event_manager.rb
@@ -2,6 +2,7 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 
 civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
