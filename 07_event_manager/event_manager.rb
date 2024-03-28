1bf83aaf734f6e84d357379b8bd9ed803b527120
Guilherme Nunes <guilherme.augd@gmail.com>
2024-03-28 11:12:33 -0300

    implement map method

--- 07_event_manager/event_manager.rb
+++ 07_event_manager/event_manager.rb
@@ -28,6 +28,8 @@ contents.each do |row|
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     )  
     legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
