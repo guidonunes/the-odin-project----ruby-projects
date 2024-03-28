bca41477f7beaf254d7011e874a5a1a85c52ba7f
Guilherme Nunes <guilherme.augd@gmail.com>
2024-03-28 11:15:50 -0300

    implement join method

--- 07_event_manager/event_manager.rb
+++ 07_event_manager/event_manager.rb
@@ -30,6 +30,8 @@ contents.each do |row|
     legislators = legislators.officials
 
     legislator_names = legislators.map(&:name)
+
+    legislators_string = legislator_names.join(", ")
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
