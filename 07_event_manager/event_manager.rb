3417461ed29659838ad4bb9bd27cfd096a181cdd
Guilherme Nunes <guilherme.augd@gmail.com>
2024-03-29 17:36:02 -0300

    update begin block

--- 07_event_manager/event_manager.rb
+++ 07_event_manager/event_manager.rb
@@ -12,14 +12,11 @@ def legislators_by_zipcode(zipcode)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )  
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislators_string = legislator_names.join(", ")
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
