c458d1ef2730259703f7ac5e47d308e80c532151
Guilherme Nunes <guilherme.augd@gmail.com>
2024-04-01 14:15:06 -0300

    fix typo

--- 07_event_manager/event_manager.rb
+++ 07_event_manager/event_manager.rb
@@ -7,7 +7,7 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end 
 
-def legislators_by_zipcode(zipcode)
+def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
@@ -52,5 +52,3 @@ contents.each do |row|
     
 end
 
-
-#TO BE CONTINUED: outputting form letters to a file
