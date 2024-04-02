55f6b3a5716fb3a589d6e8c28ea4c606399cdc6f
Guilherme Nunes <guilherme.augd@gmail.com>
2024-04-02 07:45:22 -0300

    fix typo

--- 07_event_manager/lib/event_manager.rb
+++ 07_event_manager/lib/event_manager.rb
@@ -12,7 +12,7 @@ def legislators_by_zipcode(zip)
 
   begin
     civic_info.representative_info_by_address(
-      address: zipcode,
+      address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     ).officials
